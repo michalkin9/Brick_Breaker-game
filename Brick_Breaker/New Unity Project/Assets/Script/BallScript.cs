1d894b0b61c5d033be65b8359707cc038cf0637a
Michal Yelkin <michalkin9@gmail.com>
2020-05-19 20:01:08 +0300

    paddle and ball

---
+++ Brick_Breaker/New Unity Project/Assets/Script/BallScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay; // ture - moving around . false - ball to be on paddle.
+    public Transform paddle;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // get the rigid body reference
+
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inPlay) //if false..
+        {
+            transform.position = paddle.position;
+        }
+
+        if (Input.GetButtonDown("Jump") && !inPlay ) 
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed); // gives the ball force for begging. 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //runs evert time the objuect collides the collider
+    {
+        if (collision.CompareTag("bottom"))//if the tag is bottom 
+        {
+            Debug.Log("Ball hits bottom edge of the screen"); //test detection
+           
+            rb.velocity = Vector2.zero; //kill momentom of the object.
+            inPlay = false; //will starts the ball follow the paddle;
+
+
+        }
+    }
+}
---
+++ Brick_Breaker/New Unity Project/Assets/Script/PaddleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+
+        if(transform.position.x < -5.5f)
+        {
+            transform.position = new Vector2(-5.5f, transform.position.y);
+
+        } else if (transform.position.x > 5.5f)
+        {
+            transform.position = new Vector2(5.5f, transform.position.y);
+        }
+    }
+}
